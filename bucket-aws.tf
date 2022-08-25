19c427f00b0df70ab1ff190f657761ad86006800
willcsilva <111435044+willcsilva@users.noreply.github.com>
2022-08-25 17:33:03 -0300

    AWS-BUCKET

---
+++ bucket-aws.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "second_bucket" {
+  bucket = "bucket-nethub-tf"
+}
---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.73.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      owner       = "Nethub"
+      mangaged-by = "Terraform"
+    }
+  }
+}
